a7f956cdb9bc6266f2ed1a1a02efbb7389c3b067
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-24 14:54:37 -0400

    Full stub for CLI. Though it runs twice for some reason.

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,7 +2,7 @@ require_relative '../config/environment.rb'
 
 class CLI
 
-  def run
+  def _run
     puts "Here are some audiobooks currently available for download at NYPL:"
     nypl_url = "./fixtures/available-now-list/available-now.htm"
     
@@ -15,4 +15,23 @@ class CLI
     }
   end
 
+  def run
+    puts "Here are some audiobooks currently available for download at NYPL:"
+    puts ""
+    puts "1. Who Was Rosa Parks? - Yona Zeldia McDonough"
+    puts "2. Frostbite - Richelle Mead"
+    puts ""
+    puts "Enter the number for a book you'd like to know more about:"
+    num = gets.strip.to_i # TO-DO: Validate input
+    puts ""
+    puts "Who Was Rosa Parks? (2016)"
+    puts "by Yona Zeldia McDonough"
+    puts "Length: 1:08:56"
+    puts ""
+    puts 'In 1955, Rosa Parks refused to give her bus seat to a white passenger in Montgomery, Alabama. This seemingly small act triggered civil rights protests across America and earned Rosa Parks the title "Mother of the Civil Rights Movement."'
+    puts ""
+  end
+
 end
+
+CLI.new.run
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-# describe 'CLI.run' do
-#   it 'puts "hi there"' do
-#     allow($stdout).to receive(:puts)
-#     # expect($stdout).to receive(:puts).with("hi there")
-#     CLI.new.run
-#   end
-# end
+describe 'CLI.run' do
+  it 'puts "hi there"' do
+    allow($stdout).to receive(:puts)
+    expect($stdout).to receive(:puts).with("hi there")
+    CLI.new.run
+  end
+end
