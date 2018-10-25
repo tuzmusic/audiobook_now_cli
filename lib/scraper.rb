c6d21050cea045d71369d4915fe2da7ccb42f25b
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-25 10:35:39 -0400

    slightly refactored (or at least the comments are gone)

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,29 +2,15 @@
 
 class Scraper
 
-  def self.scrape_book_list(url)
-    
+  def self.scrape_book_list(url)    
     session = Capybara::Session.new(:poltergeist)
     session.visit(url)
-
-    html = open(url)
-    doc = Nokogiri::HTML(html)
-
-    # books = doc.css('.js-titleCard')
     books = session.all('.js-titleCard')
     books_hash = books.map { |book|
       hash = {}
-
-      # hash[:title] = book.css('.title-name').first.text.strip
       hash[:title] = book.first('.title-name').text.strip
-
-      # hash[:author] = book.css('[aria-label^="Search by author"]').text.strip
       hash[:author] = book.first('[aria-label^="Search by author"]').text.strip
-      
-      # url_elem = title_elem.css('a').first
-      # hash[:url] = url_elem['href']
       hash[:url] = book.first('.title-name').first('a')['href']
-      # binding.pry
       hash 
     } 
     books_hash
